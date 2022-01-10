a3e9360b56716f923d9213a95f12da5516e33c87
Nando <57635041+nando520024@users.noreply.github.com>
2022-01-10 14:28:16 +0100

    Removed comment and changed consolemessage

--- Faultify.TestRunner/MutationTestProject.cs
+++ Faultify.TestRunner/MutationTestProject.cs
@@ -70,7 +70,6 @@ namespace Faultify.TestRunner
             }
 
             // Copy project N times
-            //progressTracker.LogBeginProjectDuplication(_parallel);
             var testProjectCopier = new TestProjectDuplicator(Directory.GetParent(projectInfo.AssemblyPath).FullName);
 
             testProjectCopier.MakeInitialCopy(projectInfo);
